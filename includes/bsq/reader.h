20a15e7c15334e966d573e96dbb028b3c8d6defc
Muhammed Ali Bekdas <mbekdas@k1m22s04.42istanbul.com.tr>
2023-02-23 15:32:56 +0300

    added strings lib

--- includes/bsq/reader.h
+++ includes/bsq/reader.h
@@ -6,7 +6,7 @@
 /*   By: 42istanbul <42istanbul.com.tr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 15:21:38 by 42istanbu         #+#    #+#             */
-/*   Updated: 2023/02/23 15:31:26 by 42istanbu        ###   ########.tr       */
+/*   Updated: 2023/02/23 15:32:41 by 42istanbu        ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef READER_H
@@ -16,6 +16,6 @@
 # include <fcntl.h>
 # include <unistd.h>
 # include "types.h"
-
+# include "strings.h"
 t_info	*read_from_file(char *file_name);
 #endif
