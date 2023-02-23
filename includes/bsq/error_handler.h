473acec9d50c2cc69e7cc8eea208c42549489e48
Muhammed Ali Bekdas <mbekdas@k1m20s07.42istanbul.com.tr>
2023-02-23 10:41:21 +0300

    error_messages integrated

---
+++ includes/bsq/error_handler.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_handler.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: 42istanbul <42istanbul.com.tr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/23 10:34:16 by 42istanbu         #+#    #+#             */
+/*   Updated: 2023/02/23 10:40:56 by 42istanbu        ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ERROR_HANDLER_H
+# define ERROR_HANDLER_H
+# define TOO_MANY_ARG "Error! Too many arguments"
+# define MISSING_FILE "Error! Missing file"
+# define MAP_ERROR "Error! Corrupt map file"
+#endif
